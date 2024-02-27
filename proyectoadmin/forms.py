ddf22ac634ed021334d293e5f1d0c0379f57b367
KirihaDC <102496825+KirihaDC@users.noreply.github.com>
2024-02-27 17:34:44 -0600

    Delete proyectoadmin directory

--- proyectoadmin/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class UserRegistrationForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    # Add other fields as needed
-
-
