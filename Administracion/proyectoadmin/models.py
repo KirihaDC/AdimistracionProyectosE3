4e41055fdaf3b1bd8782fd82b124dec50bac2df4
KirihaDC <102496825+KirihaDC@users.noreply.github.com>
2024-02-27 14:33:20 -0600

    Elimiar carpeta por otra
    Se va a cambiar otro proyecto mejor estructurado y con un txt con comandos

--- Administracion/proyectoadmin/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
