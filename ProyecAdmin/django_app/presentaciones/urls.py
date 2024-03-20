d665289bf89eb2be023adb79d1ca0c7b8363c61a
EduardoSantana <37180283@uaz.edu.mx>
2024-03-19 22:19:54 -0600

    Se logro que el boton Generar mande mensaje

--- ProyecAdmin/django_app/presentaciones/urls.py
+++ ProyecAdmin/django_app/presentaciones/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #path('archivos/', PresentacionListView.as_view(), name='archivo_texto_list'),
     path('presentaciones/', views.presentaciones, name='presentaciones'),
+    path('crear_archivo/', views.crear_archivo, name='crear_archivo'),
 ]
--- ProyecAdmin/django_app/presentaciones/views.py
+++ ProyecAdmin/django_app/presentaciones/views.py
@@ -3,6 +3,9 @@ from .models import Presentacion
 from django.views.generic import ListView
 from .forms import CrearArchivoForm
 from .forms import CrearArchivoForm
+from django.http import HttpResponse, JsonResponse
+import os
+
 #
 from django.views.generic import ListView
 #
@@ -35,15 +38,19 @@ def crear_archivo(request):
     if request.method == 'POST':
         form = CrearArchivoForm(request.POST)
         if form.is_valid():
-            nombre_archivo = form.cleaned_data['nombre_archivo']
-            contenido = form.cleaned_data['contenido']
-            ruta_archivo = f"/ProyecAdmin/django_app/PresentacionesTXT/{nombre_archivo}.txt"  # Ruta predefinida con el nombre asignado
-            with open(ruta_archivo, 'w') as archivo:
-                archivo.write(contenido)
-            return render(request, 'archivo_creado.html', {'ruta_archivo': ruta_archivo})
+            nombre_archivo = request.POST.get('titulo')
+            contenido = request.POST.get('contenido')
+            ruta_proyecto = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            ruta_archivo = os.path.join(ruta_proyecto, 'PresentacionesTXT', '{nombre_archivo}.txt')
+            try:
+                with open(ruta_archivo, 'w') as archivo:
+                    archivo.write(contenido)
+                return JsonResponse({'mensaje': 'Archivo guardado correctamente.'})
+            except Exception as e:
+                return JsonResponse({'mensaje': f'Error al guardar el archivo: {str(e)}'}, status=500)            
     else:
-        form = CrearArchivoForm()
-    return render(request, 'crear_archivo.html', {'form': form})
+        #return JsonResponse({'mensaje': 'Método no permitido.'}, status=405)
+        return render(request, 'crear_archivo.html', {'form': form})
 
 def presentaciones(request):
     return render(request, 'Presentaciones.html')
