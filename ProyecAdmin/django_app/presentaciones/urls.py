35d64edcd70d4b526c4710b6f40f9c4274512320
ANDRÉS CONTRERAS SANCHEZ <42102027@uaz.edu.mx>
2024-03-19 19:08:05 -0600

    Se suben la vista de las presentaciones, con controles de mouse y teclado

--- ProyecAdmin/django_app/presentaciones/urls.py
+++ ProyecAdmin/django_app/presentaciones/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #path('archivos/', PresentacionListView.as_view(), name='archivo_texto_list'),
     path('presentaciones/', views.presentaciones, name='presentaciones'),
+    path('lista_presentaciones/', views.lista_presentaciones, name='lista_presentaciones')
 ]
