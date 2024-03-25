315c524acbc8c231193f664a838cc8cb51123668
«KirihaDC» <kirihadc020@gmail.com>
2024-03-25 16:32:46 -0600

    Templates y ajustes de codigo

--- ProyecAdmin/django_app/presentaciones/urls.py
+++ ProyecAdmin/django_app/presentaciones/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     #path('archivos/', PresentacionListView.as_view(), name='archivo_texto_list'),
     path('presentaciones/', views.presentaciones, name='presentaciones'),
     path('crear_archivo/', views.crear_archivo, name='crear_archivo'),
-    path('lista_presentaciones/', views.lista_presentaciones, name='lista_presentaciones')
+    path('lista_presentaciones/', views.lista_presentaciones, name='lista_presentaciones'),
+    path('editar_presentacion/', views.editar_presentacion, name='editar_presentacion'),
+    path('eliminar_archivo/<str:nombre_archivo>/', views.eliminar_archivo, name='eliminar_archivo'),
+    path('ver_presentacion/', views.ver_presentacion, name='ver_presentacion'),
+
 ]
--- ProyecAdmin/django_app/presentaciones/views.py
+++ ProyecAdmin/django_app/presentaciones/views.py
@@ -6,7 +6,9 @@ from .forms import CrearArchivoForm
 from django.http import HttpResponse, JsonResponse
 import os
 from django.conf import settings
+from django.shortcuts import redirect
 from django.contrib import messages
+from pathlib import Path
 
 #
 from django.views.generic import ListView
@@ -68,6 +70,12 @@ def crear_archivo(request):
 def presentaciones(request):
     return render(request, 'Presentaciones.html')
 
+def editar_presentacion(request):
+    return render(request, 'editar_presentacion.html')
+
+def ver_presentacion(request):
+    return render(request, 'ver_presentacion.html')
+
 def lista_presentaciones(request):
     # Directorio donde se encuentran los archivos de texto
     directorio = os.path.join(settings.BASE_DIR, 'PresentacionesTXT')
@@ -77,3 +85,53 @@ def lista_presentaciones(request):
 
     # Pasar la lista de archivos a la plantilla
     return render(request, 'presentacion_list.html', {'archivos': archivos})
+
+def eliminar_archivo(request, nombre_archivo):
+    ruta_archivo = os.path.join(settings.BASE_DIR, 'PresentacionesTXT', nombre_archivo)
+    if os.path.exists(ruta_archivo):
+        os.remove(ruta_archivo)
+        return redirect('lista_presentaciones')
+    else:
+        return redirect('lista_presentaciones')
+
+def editar_presentacion(request):
+    nombre_archivo = request.GET.get('archivo', '')  
+    ruta_directorio = Path(settings.BASE_DIR) / 'PresentacionesTXT'
+    ruta_archivo = ruta_directorio / nombre_archivo
+
+    try:
+        if request.method == 'POST':
+            titulo_presentacion = request.POST.get('tituloPresentacion', '')
+            contenido_presentacion = request.POST.get('contenidoPresentacion', '')
+
+            # Verificar si tanto el título como el contenido están presentes
+            if titulo_presentacion and contenido_presentacion:
+                # Verificar si el archivo existe antes de intentar guardarlo
+                if os.path.exists(ruta_archivo):
+                    # Sobrescribir el archivo con el nuevo título y contenido
+                    with open(ruta_archivo, 'w', encoding='utf-8') as archivo:
+                        archivo.write(contenido_presentacion)
+
+                    # Renombrar el archivo si el título ha cambiado
+                    nuevo_nombre = ruta_directorio / (titulo_presentacion + '.txt')
+                    if nuevo_nombre != ruta_archivo:
+                        os.rename(ruta_archivo, nuevo_nombre)
+
+                    return redirect('lista_presentaciones')
+                else:
+                    return HttpResponse("El archivo no existe.")
+            else:
+                return HttpResponse("El título y el contenido de la presentación son necesarios.")
+
+        elif request.method == 'GET':
+            if os.path.exists(ruta_archivo):
+                with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+                    contenido = archivo.read()
+                titulo = os.path.splitext(nombre_archivo)[0]
+            else:
+                contenido = ""
+                titulo = ""
+
+            return render(request, 'editar_presentacion.html', {'nombre_archivo': nombre_archivo, 'contenido': contenido, 'titulo': titulo})
+    except (FileNotFoundError, PermissionError) as e:
+        return HttpResponse(f"Error: {e}")
