a189812b5a476744fc6a86147ca5294563805d95
42102028 <112010457+42102028@users.noreply.github.com>
2024-05-27 12:32:04 -0600

    Añadidas rutas para creación y edición de presentaciones

--- ProyecAdmin/django_app/presentaciones/urls.py
+++ ProyecAdmin/django_app/presentaciones/urls.py
@@ -3,12 +3,12 @@ from . import views
 from usuarios.views import administrador
 
 urlpatterns = [
-    #path('archivos/', PresentacionListView.as_view(), name='archivo_texto_list'),
     path('presentaciones/', views.presentaciones, name='presentaciones'),
     path('crear_archivo/', views.crear_archivo, name='crear_archivo'),
     path('lista_presentaciones/', views.lista_presentaciones, name='lista_presentaciones'),
     path('editar_presentacion/', views.editar_presentacion, name='editar_presentacion'),
     path('eliminar_archivo/<str:nombre_archivo>/', views.eliminar_archivo, name='eliminar_archivo'),
     path('ver_presentacion/', views.ver_presentacion, name='ver_presentacion'),
+    path('upload/', views.upload_file, name='upload_file'),  # Nueva ruta para la subida de archivos
     path('administrador/', administrador, name='administrador'),
 ]
